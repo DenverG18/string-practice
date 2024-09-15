1185b9d2a04832324b9db1dfe71648bddb023099
Your Name <you@example.com>
2024-09-15 09:55:29 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let MyFavActorfirstname = 'Tom';
+console.log('First Name: ' + MyFavActorfirstname);
+
+let MyFavActorlastname ='Holland';
+console.log('Last Name: ' , MyFavActorlastname);
+
+//CONCATENATION
+let fullName = MyFavActorfirstname + ' ' +MyFavActorlastname;
+console.log('Full Name: ' , fullName);
+
+let favActorname = 'Tom Holland';
+console.log('Uppercase: ' , favActorname.toLocaleUpperCase());
+
+let favActor = 'Tom Holland';
+console.log('Lowercase',favActor.toLowerCase());
+
+let LowerCase = 'oreo';
+console.log('Fav Pet Name ' , LowerCase.toLocaleUpperCase());
+
+//APPEND
+let message = 'My Favorite Actor is  TOM HOLLAND';
+message +=' His best show is Spider Man';
+console.log(message);
+
+
